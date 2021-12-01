76d5b6b6cfa670a6a3ab93ab65e86db67b7cdd21
Andrew Flemming <andrew@flemming.cc>
2021-12-01 15:09:02 -0500

    Publishing from Elvira

--- resources/mtf-publish-elvira.sh
+++ resources/mtf-publish-elvira.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
 
+# Pull the most recent changes from Github
 git pull
 
+# Always perform a clean build by first deleting the publishing directory
 rm -rf docs
 
+# Build the site
 /opt/homebrew/bin/hugo -D
 
+# Make sure the CNAME doc is present (tells Github to use http://makethefinal.ca)
 if ! [ -e docs/CNAME ]
 then
   echo "CNAME not found, copying from resources"
   cp resources/CNAME docs/CNAME
 fi
- 
+
+# Stage all changes
 git add -A
+
+# Commit the changes
 git commit -m "Publishing from Elvira"
+
+# Push the changes up to Github
 git push
